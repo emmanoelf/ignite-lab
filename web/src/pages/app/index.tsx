fe3a98a8902b8678ccb1fb37f05877e638c6672d
Emmanoel F <emmanoel_95@hotmail.com>
2022-05-18 21:59:23 -0300

    scheema prisma/migration/index.ts

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,4 +1,5 @@
-import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getAccessToken, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
 
 export default function Home(){
     const {user} = useUser()
