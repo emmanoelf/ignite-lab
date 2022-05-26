10f6c14fe9a225ac15942eba7827eb48a28acb6f
Emmanoel F <emmanoel_95@hotmail.com>
2022-05-25 22:29:18 -0300

    feat: add new purchase

--- purchases/src/http/graphql/resolvers/purchases.resolver.ts
+++ purchases/src/http/graphql/resolvers/purchases.resolver.ts
@@ -1,8 +1,18 @@
 import { UseGuards } from '@nestjs/common';
-import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+import { CustomersService } from 'src/services/customers.service';
 import { ProductsService } from 'src/services/products.service';
 import { PurchasesService } from 'src/services/purchases.service';
+import { CreatePurchaseInput } from '../inputs/create-purchase-input';
 import { Product } from '../models/product';
 import { Purchase } from '../models/purchase';
 
@@ -11,6 +21,7 @@ export class PurchasesResolver {
   constructor(
     private purchasesService: PurchasesService,
     private productsService: ProductsService,
+    private customersService: CustomersService,
   ) {}
 
   @Query(() => [Purchase])
@@ -22,5 +33,27 @@ export class PurchasesResolver {
   @ResolveField(() => Product)
   product(@Parent() purchase: Purchase) {
     return this.productsService.getProductById(purchase.productId);
+  }
+
+  @Mutation(() => Purchase)
+  @UseGuards(AuthorizationGuard)
+  async createPurchase(
+    @Args('data') data: CreatePurchaseInput,
+    @CurrentUser() user: AuthUser,
+  ) {
+    let customer = await this.customersService.getCustomerByAuthUserId(
+      user.sub,
+    );
+
+    if (!customer) {
+      customer = await this.customersService.createCustomer({
+        authUserId: user.sub,
+      });
+    }
+
+    return this.purchasesService.createPurchase({
+      productId: data.productId,
+      customerId: customer.id,
+    });
   }
 }
--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
+interface CreatePurchaseParams {
+  customerId: string;
+  productId: string;
+}
+
 @Injectable()
 export class PurchasesService {
   constructor(private prisma: PrismaService) {}
@@ -10,6 +15,25 @@ export class PurchasesService {
     return await this.prisma.purchase.findMany({
       orderBy: {
         createdAt: 'desc',
+      },
+    });
+  }
+
+  async createPurchase({ customerId, productId }: CreatePurchaseParams) {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new Error('Product not found.');
+    }
+
+    return await this.prisma.purchase.create({
+      data: {
+        customerId,
+        productId,
       },
     });
   }
